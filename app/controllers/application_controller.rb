11c447dd5fc5b68c29dcf15ccd65a99a260b81d9
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-10-12 21:31:01 +0900

    Create user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    return @current_user if @current_user
+
+    @current_user =
+      if session[:user_id]
+        User.find(session[:user_id])
+      else
+        User.create.tap do |user|
+          session[:user_id] = user.id
+        end
+      end
+  end
 end
