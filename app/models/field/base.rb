4e30d7386225f84a3a09637a339036604318145c
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-10-14 22:53:11 +0900

    Dependent destroy

--- app/models/field/base.rb
+++ app/models/field/base.rb
@@ -2,5 +2,5 @@ class Field::Base < ActiveRecord::Base
   self.table_name = 'fields'
 
   belongs_to :game
-  has_many :tiles, foreign_key: :field_id
+  has_many :tiles, foreign_key: :field_id, dependent: :destroy
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-  has_many :hands, class_name: 'Field::Hand'
-  has_one :wall, class_name: 'Field::Wall'
+  has_many :hands, class_name: 'Field::Hand', dependent: :destroy
+  has_one :wall, class_name: 'Field::Wall', dependent: :destroy
 end
