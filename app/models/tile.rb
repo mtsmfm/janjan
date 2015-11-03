fe39ed34061302bbf52543065049f0bdc7954d0a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-11-03 18:20:41 +0900

    Unify suit names
    - wan -> man
    - ton..pei -> east..north

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,6 +1,13 @@
 class Tile < ActiveRecord::Base
   # https://en.wikipedia.org/wiki/Japanese_Mahjong#General_mahjong_rules
   KINDS = %i(
+    east
+    south
+    west
+    north
+    chun
+    hatsu
+    haku
     man_1
     man_2
     man_3
@@ -10,15 +17,6 @@ class Tile < ActiveRecord::Base
     man_7
     man_8
     man_9
-    pin_1
-    pin_2
-    pin_3
-    pin_4
-    pin_5
-    pin_6
-    pin_7
-    pin_8
-    pin_9
     sou_1
     sou_2
     sou_3
@@ -28,13 +26,15 @@ class Tile < ActiveRecord::Base
     sou_7
     sou_8
     sou_9
-    ton
-    nan
-    sha
-    pei
-    haku
-    hatsu
-    chun
+    pin_1
+    pin_2
+    pin_3
+    pin_4
+    pin_5
+    pin_6
+    pin_7
+    pin_8
+    pin_9
   )
 
   belongs_to :game
