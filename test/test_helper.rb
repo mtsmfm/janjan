87f19ae83742fddd8664ea4a9c4bbac59d7b0115
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-11-14 02:00:32 +0900

    Use pry-rescue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'mocha/mini_test'
+require 'pry-rescue/minitest'
 
 Rake::Task['db:test:prepare'].invoke
 
