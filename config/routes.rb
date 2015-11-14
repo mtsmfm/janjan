277d94c589d7324803adf2bf45b473568b9f60f8
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-11-15 02:00:24 +0900

    Restore debug

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
 
   match '/websocket', to: ActionCable.server, via: [:get, :post]
 
+  get :debug, to: 'debug#index' if Rails.env.development?
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
