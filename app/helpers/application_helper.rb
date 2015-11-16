aaad2d5f8457fc160e8d7b0b3d06d293cff46ff3
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-11-16 20:32:05 +0900

    button_to_action_if_able

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def button_to_action_if_able(type:, user:, room:, params: {})
+    if "Action::#{type.to_s.classify}".constantize.able?(user: user, room: room)
+      button_to type.to_s.classify, room_actions_path(room, {type: type}.merge(params))
+    end
+  end
 end
