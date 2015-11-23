247f3553ad4f85acf0391335c6b96db2ac3c599a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-11-24 00:40:40 +0900

    Collect point

--- app/models/action/self_pick.rb
+++ app/models/action/self_pick.rb
@@ -9,6 +9,35 @@ class Action::SelfPick < Action::Base
   end
 
   def act!(params:)
+    # TODO calc
+    self.base_point = 2000
+
+    win_point = game.seats.reject {|s| s == seat }.sum do |s|
+      if seat.east?
+        collect_point!(seat: s, point: ceil(base_point * 2))
+      else
+        if s.east?
+          collect_point!(seat: s, point: ceil(base_point * 2))
+        else
+          collect_point!(seat: s, point: ceil(base_point))
+        end
+      end
+    end
+
+    seat.update!(point: seat.point + win_point)
+
     save!
+  end
+
+  private
+
+  def ceil(point)
+    (point / 100r).ceil * 100
+  end
+
+  def collect_point!(point:, seat:)
+    seat.update!(point: seat.point - point)
+
+    point
   end
 end
