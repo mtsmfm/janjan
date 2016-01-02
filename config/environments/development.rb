b6115d62505d2077f0faf856c7e9103bbdaeb11a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-02 17:46:38 +0900

    Restore x-frame-options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_cable.disable_request_forgery_protection = true
+
+  config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOWALL'
 end
