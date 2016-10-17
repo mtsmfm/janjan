c2e5dc6403f11abb24ea0e93a25f8b48b35a099c
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-10-18 01:21:10 +0900

    Disable minify at this time

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -54,10 +54,12 @@ var config = {
 if (production) {
   config.plugins.push(
     new webpack.NoErrorsPlugin(),
+    /*
     new webpack.optimize.UglifyJsPlugin({
       compressor: { warnings: false },
       sourceMap: false
     }),
+    */
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') }
     }),
