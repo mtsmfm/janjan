9d8a1dadd293e37f5e436601114c6002d2f69a4e
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-05-07 22:32:06 +0900

    wip

--- app/graphql/types/tile_type.rb
+++ app/graphql/types/tile_type.rb
@@ -1,3 +1,6 @@
 Types::TileType = GraphQL::ObjectType.define do
   name "Tile"
+  field :id, !types.ID
+  field :kind, types.String
+  field :order, types.Int
 end
--- app/graphql/types/wall_type.rb
+++ app/graphql/types/wall_type.rb
@@ -1,5 +1,4 @@
 Types::WallType = GraphQL::ObjectType.define do
   name "Wall"
-
   field :tiles, !types[Types::TileType]
 end
